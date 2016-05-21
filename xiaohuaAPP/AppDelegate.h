fecc5a5c28ad54079efa25b3126575993ffaaf1e
大兵_2014 <xiaolu96@126.com>
2016-05-21 16:45:05 +0800

    Initial Commit

---
+++ xiaohuaAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiaohuaAPP
+//
+//  Created by fengbing on 16/5/21.
+//  Copyright © 2016年 fengbing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiaohuaAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xiaohuaAPP
+//
+//  Created by fengbing on 16/5/21.
+//  Copyright © 2016年 fengbing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
