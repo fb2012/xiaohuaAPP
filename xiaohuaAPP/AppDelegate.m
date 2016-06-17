7a492e18cafdc26da47cb6b732109f07d96a7072
大兵_2014 <xiaolu96@126.com>
2016-06-17 23:51:12 +0800

    dd

--- xiaohuaAPP/AppDelegate.m
+++ xiaohuaAPP/AppDelegate.m
@@ -8,7 +8,8 @@
 
 #import "AppDelegate.h"
 #import "FBTabbarViewController.h"
-
+#import <ifaddrs.h>
+#import <arpa/inet.h>
 @interface AppDelegate ()
 
 @end
@@ -17,6 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self getIPAddress];
     // Override point for customization after application launch.
     FBTabbarViewController *showVC=[[FBTabbarViewController alloc]init];
     self.window.rootViewController=showVC;
@@ -46,5 +48,32 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+// Get IP Address
+- (NSString *)getIPAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    DLog(@"ip==%@",address);
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;}
 
 @end
